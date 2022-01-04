66813d5fc88656b0cd210d0c3093be0089fc02aa
youwah <cywah@hotmail.com>
2022-01-04 21:20:20 +0800

    update11

--- PKOB/test1.py
+++ PKOB/test1.py
@@ -1,1 +1,3 @@
 #   path('<int:question_id>/', views.detail, name='detail'),
+#   path123('<int:question_id>/', views.detail, name='detail'),
+#   path456('<int:question_id>/', views.detail, name='detail'),
